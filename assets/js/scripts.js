22461255aea7244b201c964c945297f5cd32f08c
GekaMa <jobmailhtml@gmail.com>
2024-11-09 14:33:54 +0200

    added project

---
+++ assets/js/scripts.js
@@ -0,0 +1,67 @@
+// modals for company profile page
+const blocks = document.querySelectorAll(".company-profile__call");
+const modals = document.querySelectorAll(".modal");
+const closeButtons = document.querySelectorAll(".modal__header-btn");
+
+blocks.forEach((block) => {
+  block.addEventListener("click", () => {
+    const modalId = block.getAttribute("data-modal");
+    const modal = document.getElementById(modalId);
+    if (modal) {
+      modal.style.display = "block";
+    }
+  });
+});
+
+closeButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    button.closest(".modal").style.display = "none";
+  });
+});
+
+window.addEventListener("click", (event) => {
+  modals.forEach((modal) => {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+});
+
+// favorites
+const stars = document.querySelectorAll(".favorites");
+
+for (let n = 0; n < stars.length; n++) {
+  stars[n].addEventListener("click", function () {
+    this.parentElement.classList.toggle("favorites--active");
+  });
+}
+
+// option-box
+const optionBox = document.querySelectorAll(".options-box__btn");
+
+if(optionBox) {
+  for (let m = 0; m < optionBox.length; m++) {
+    optionBox[m].addEventListener("click", function () {
+      this.parentElement.classList.toggle("options-box--active");
+    });
+  }
+
+  window.addEventListener("click", (event) => {
+    optionBox.forEach((item) => {
+      if (event.target === item) {
+        item.classList.remove("options-box--active");
+      }
+    });
+  });
+}
+
+// header btn
+const headerBtn = document.querySelector(".header__btn");
+const header = document.querySelector(".header");
+
+if (header) {
+  headerBtn.addEventListener("click", function () {
+    this.classList.toggle("header__btn--active");
+    header.classList.toggle("header--active");
+  });
+}
